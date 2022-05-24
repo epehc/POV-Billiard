2791bcaa8da06bda04cce4ba914325493cec08b3
Jose Cardona <62968561+epehc@users.noreply.github.com>
2022-05-24 17:41:12 +0200

    first commit - template files
    Added template files to repository

---
+++ Billiard.cpp
@@ -0,0 +1,160 @@
+/* ---------------------------------------------------------------
+   name:           Beleg.cpp
+   purpose:        Beleg Template
+   version:	   STARTER CODE
+   author:         katrin lang
+                   htw berlin
+   ------------------------------------------------------------- */
+
+#include "GLIncludes.hpp"
+
+#include <iostream> 
+
+#include "Billiard.hpp"
+#include "Context.hpp"
+#include "Input.hpp"
+
+#define GLM_FORCE_RADIANS
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+// use this with care
+// might cause name collisions
+using namespace glm;
+using namespace std;
+
+// field of view
+GLfloat Billiard::fov= 45.0;
+GLfloat Billiard::cameraZ= 3;
+
+mat4 Billiard::projectionMatrix, Billiard::viewMatrix, Billiard::modelMatrix(1);
+
+LightSource Billiard::lightSource={
+    // position
+  glm::vec4(0, 2, 3, 1),
+  // ambient color
+  glm::vec4(1.0, 1.0, 1.0, 1.0),
+  // diffuse color
+  glm::vec4(1.0, 1.0, 1.0, 1.0),
+  // specular color
+  glm::vec4(1.0, 1.0, 1.0, 1.0),
+};
+
+void Billiard::init(){
+  
+  // set background color to black
+  glClearColor(0.0,0.0,0.0,1.0);
+  glPolygonMode(GL_FRONT, GL_FILL);
+    
+  // enable depth test (z-buffer)
+  glDepthFunc(GL_LESS);
+  glEnable(GL_DEPTH_TEST);
+
+  // enable antialiasing
+  glEnable(GL_MULTISAMPLE);
+}
+
+// adjust to new window size
+void Billiard::reshape(){
+    
+  // viewport
+  glViewport(0, 0, (GLsizei) window->width(), (GLsizei) window->height());
+
+  computeViewMatrix();
+  computeProjectionMatrix();
+}
+
+void Billiard::computeViewMatrix(void){
+
+  viewMatrix= glm::lookAt(vec3(0,0,cameraZ), vec3(0), vec3(0,1,0));
+}
+
+void Billiard::computeProjectionMatrix(void){
+
+  // reshaped window aspect ratio
+  float aspect= (float) window->width() / (float) window->height();
+  
+  // compute near and far plane
+  float nearPlane=cameraZ/10.0f; 
+  float farPlane= cameraZ*10.0f;
+  
+  projectionMatrix= glm::perspective(radians(fov), aspect, nearPlane, farPlane);
+}
+
+// this is where the drawing happens
+void Billiard::display(void){
+  
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  
+  // display back buffer
+  window->swapBuffers();
+}
+
+// keyboard control
+void Billiard::keyPressed(){
+  
+  // rotate selected node around 
+  // x,y and z axes with keypresses
+  switch(keyboard->key){
+    
+  case 'q':
+  case 'Q':
+    exit(0);
+
+  default:
+    break;
+  }
+}
+
+// keyboard callback for special keys
+// (arrow keys for node selection)
+void Billiard::specialKey(){
+
+  // rotate selected node around 
+  // x,y and z axes with keypresses
+  switch(keyboard->code) {
+
+  case Keyboard::Code::UP:
+    
+    break;
+  case Keyboard::Code::DOWN:
+    
+    break;
+  case Keyboard::Code::LEFT:
+    
+    break;
+  case Keyboard::Code::RIGHT:
+    
+    break;
+  default:
+    break;
+  }
+}
+
+vector< pair < int, string > > Billiard::menuEntries{{Menu::QUIT, "quit"}};
+
+// mouse menu control
+void Billiard::menu(int id){
+  
+  switch (id) {
+  case Menu::QUIT: 
+    exit(0);
+
+  default:
+    break;
+  }
+}
+
+int main(int argc, char** argv){
+
+  // initialize OpenGL context
+  OpenGLContext<Billiard>::init(&argc, argv);
+
+  // some diagnostic output
+  std::cout << "GPU: " << glGetString(GL_RENDERER) << ", OpenGL version: " << glGetString(GL_VERSION) << ", GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    
+  //start event loop
+  InputManager<Billiard>::startEventLoop();
+  
+  return 0;
+}
---
+++ Billiard.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+/* ----------------------------------------------------------------
+   name:           Control.hpp
+   purpose:        robot control
+   version:	   SKELETON CODE
+   TODO:           add more callbacks (see Application.hpp)
+   author:         katrin lang
+   computer graphics
+   tu berlin
+   ------------------------------------------------------------- */
+
+#include <vector>
+// for pair
+#include <utility>
+
+#include "glm/glm.hpp"
+
+#include "Context.hpp"
+#include "Application.hpp"
+
+#include "LightSource.h"
+#include "Material.h"
+
+#include "TriangleMesh.hpp"
+#include "GLSL.hpp"
+
+ OpenGLConfiguration config(glm::uvec2(2, 1),
+		      OpenGLConfiguration::Profile::COMPATIBILITY,
+		      (Window::DOUBLE | Window::DEPTH | Window::RGB | Window::MULTISAMPLE),
+		      8,
+		      glm::uvec2(50, 50),
+		      glm::uvec2(512, 512),
+		      "Computer Graphics - Beleg");
+
+
+class Billiard : public OpenGLApplication<config>{
+
+public:
+  
+  // initialization
+  static void init();
+
+  // display scene
+  static void display(void);
+ 
+  // called after window rehape
+  static void reshape();
+ 
+  // called when menu item was selected          
+  static void menu(int id);
+  
+  // keyboard callback
+  static void keyPressed();
+
+  // keyboard callback for special keys 
+  static void specialKey();
+    
+  // menu entries
+  static std::vector< std::pair< int, std::string > >  menuEntries;
+  
+private:
+
+  static void computeViewMatrix(void);
+  static void computeProjectionMatrix(void);
+
+  // field of view (in degrees)                                               
+  static GLfloat fov;
+
+  static float pan;
+  
+  // camera position                                                           
+  static float cameraZ;
+
+  // near and far plane                                                        
+  static float nearPlane, farPlane;
+
+  static glm::mat4 projectionMatrix;
+  static glm::mat4 viewMatrix;
+  static glm::mat4 modelMatrix;
+
+  static LightSource lightSource;
+  static Material material;
+  
+  struct Menu{
+    enum Item{QUIT};
+  };
+
+};
